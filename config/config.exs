c31231d80987d21909f518d2b9ac0654f1f0b3ac
Fredrik Teschke <f@ftes.de>
2025-04-06 10:20:09 +0200

    mix phx.gen.auth Accounts User users

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :my_app, :scopes,
+  user: [
+    default: true,
+    module: MyApp.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: MyApp.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :my_app,
   ecto_repos: [MyApp.Repo],
   generators: [timestamp_type: :utc_datetime]
